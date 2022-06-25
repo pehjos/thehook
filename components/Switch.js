fd08d39b829347e7c6dfa3cdd6f1973b1212b7e2
pehjos <71464455+pehjos@users.noreply.github.com>
2022-06-25 17:30:30 +0000

    now

--- components/Switch.js
+++ components/Switch.js
@@ -1,93 +1,69 @@
-import React from 'react'
-import Image from 'next/image';
-import { transition } from "./transition";
-import { Badge, IconButton, Dialog, Avatar, Divider } from "@mui/material";
-import useToggle from "./useToggle";
-import Switch from './Switch'
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import FormGroup from '@mui/material/FormGroup';
 import { useTheme } from 'next-themes'
-import { useSession, signOut } from "next-auth/react";
-import { Compass,ChatDots,Search,
-PersonCircle,
-Gear
-} from 'react-bootstrap-icons';
-import { BookmarkAddedOutlined, Close, ExitToApp, ExitToAppTwoTone, ExpandMore, Help, HelpOutline, ReadMore, Settings } from '@mui/icons-material';
-function Appbar() {
-const { value, toggleValue } = useToggle(false);
-const { data: session } = useSession();
-const { theme, setTheme } = useTheme()
-return (
-<div className="topbar">
-<div className="topbar-child" >
-<div className="icon">
-<Compass size={19}   className="ml-4"/>  
-<ChatDots size={19}   className="ml-4"/>
-</div>
-<Image
-src="/lg.png"
-width={80}
-height={40}
-className="logo"
-alt="latest news"
-/>
-<div className="icon">
-<Search size={19}    className="ml-4"/>
-<PersonCircle   onClick={toggleValue} size={24}   className="ml-4"/>
-</div>
-</div>
-<Dialog className='dialog'
-fullScreen
-open={value}
-onClose={toggleValue}
-TransitionComponent={transition}
->
-  <div className='drawer_top'>
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch, { SwitchProps } from '@mui/material/Switch';
 
-<IconButton
-  edge="end"
-  color="inherit"
-  aria-label="close"
-  style={{ marginRight: "5px" }}
-  onClick={toggleValue}
->
-  <Close className="closename" />
-</IconButton> 
-<Switch />
-</div>
-<div className='profileview'>
-<div className='profileview_left'>
-<Avatar src={session?.user?.image}/>
-<h5>Pehjos</h5>
-</div>
-<div className='profileview_right'>
-<ExpandMore />
-</div>
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+  width: 62,
+  height: 34,
+  padding: 7,
+  '& .MuiSwitch-switchBase': {
+    margin: 1,
+    padding: 0,
+    transform: 'translateX(6px)',
+    '&.Mui-checked': {
+      color: '#fff',
+      transform: 'translateX(22px)',
+      '& .MuiSwitch-thumb:before': {
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+          '#fff',
+        )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+      },
+      '& + .MuiSwitch-track': {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+      },
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    backgroundColor: theme.palette.mode === 'dark' ? '#003892' : '#001e3c',
+    width: 32,
+    height: 32,
+    '&:before': {
+      content: "''",
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      left: 0,
+      top: 0,
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+        '#fff',
+      )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+    },
+  },
+  '& .MuiSwitch-track': {
+    opacity: 1,
+    backgroundColor: theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be',
+    borderRadius: 20 / 2,
+  },
+}));
 
-</div>
-<div className='menus_'>
-<div className='menus_child'>
-<BookmarkAddedOutlined/>
-<p>saved</p>
+export default function CustomizedSwitches() {
+  const { theme, setTheme } = useTheme()
+  return (
+    <div>
 
-</div>
-<div className='menus_child'>
-<Settings/>
-<p>settings</p>
-
-</div>
-<div className='menus_child'>
-<HelpOutline/>
-<p>help && feedback</p>
-</div>
-<div className='menus_child'>
-  <ExitToApp/>
-<p>signOut</p>
-</div>
-</div>
-<Divider/>
-</Dialog>  
-</div>
-)
+  <FormGroup>
+       {theme=='light'?(<FormControlLabel
+        control={<MaterialUISwitch onClick={() => setTheme('dark')} sx={{ m: 1 }} default />}
+      />):(<FormControlLabel
+        control={<MaterialUISwitch onClick={() => setTheme('light')} sx={{ m: 1 }} defaultChecked />}
+      />)}
+    </FormGroup>
+  </div>
+  );
 }
-
-export default Appbar
-// this is the first topbat that you see on top component
