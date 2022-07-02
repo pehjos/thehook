ee31fd4b092ecac41c9673acb5246a53bc14d6c6
pehjos <71464455+pehjos@users.noreply.github.com>
2022-07-02 01:39:59 +0000

    mekoda

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,8 @@ import "../styles/main.css";
 import "../styles/comment.css";
 import "../styles/account.css";
 import "../styles/onmind.css";
+import "../styles/profile.css";
+import "../styles/edit.css";
 import "../styles/buttonadd.css";
 import { ThemeProvider } from "next-themes";
 import { SessionProvider } from "next-auth/react";
@@ -15,7 +17,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <RecoilRoot>
-        <ThemeProvider >
+        <ThemeProvider storageKey = 'theme' >
           <Component {...pageProps} />
         </ThemeProvider>
       </RecoilRoot>
